edb877cea26d55c7da8736f64c1db3c3d3ee256a
Dan Ruta <ruta_dan_sebastian@yahoo.co.uk>
2017-10-14 15:20:43 +0100

    Added first Google Test test case - debug

--- test/cpp-test.cpp
+++ test/cpp-test.cpp
@@ -1,11 +1,37 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "../dev/cpp/Network.cpp"
+
+double standardDeviation (std::vector<double> arr) {
+    double avg = 0;
+
+    for (int i=0; i<arr.size(); i++) {
+        avg += arr[i];
+    }
+
+    avg /= arr.size();
+
+    for (int i=0; i<arr.size(); i++) {
+        arr[i] = pow(arr[i] - avg, 2);
+    }
+
+    double var = 0;
+
+    for (int i=0; i<arr.size(); i++) {
+        var += arr[i];
+    }
+
+    return sqrt(var / arr.size());
+}
 
 namespace {
 
-    TEST(Hello, world) {
-        EXPECT_EQ(true, true);
+    // Misc
+    TEST(standardDeviation, Calculates_value_correctly) {
+        std::vector<double> vals = {3,5,7,8,5,25,8,4};
+        EXPECT_EQ( standardDeviation(vals), 6.603739470936145 );
     }
+
 }
 
 int main (int argc, char** argv) {
